8d5e97ded3c814a5facf9e8944b041fb5748cb17
Weirddogte <epan64369@adams12.org>
2022-11-30 14:52:08 -0700

    Mecanum_wheel_drive with finsihed teleop and mecanum hardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumHardware.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -9,12 +10,20 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
 public class MecanumHardware {
     private LinearOpMode myOpMode = null;
     private DcMotor leftBack = null;
     private DcMotor rightFront = null;
     private DcMotor leftFront = null;
     private DcMotor rightBack = null;
+//    BNO055IMU imu;
+//    Orientation angles;
+
     public ElapsedTime runtime = new ElapsedTime();
 
     public MecanumHardware(LinearOpMode opmode) {
@@ -30,13 +39,24 @@ public class MecanumHardware {
     static final double TURN_SPEED = 0.5;
 
     public void init() {
+
+//        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+//        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+//        parameters.mode                = BNO055IMU.SensorMode.IMU;
+//        parameters.loggingEnabled      = true;
+//        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+//
+//        imu = myOpMode.hardwareMap.get(BNO055IMU.class, "imu");
+//        imu.initialize(parameters);
+//        myOpMode.sleep(3000);
+
         leftBack = myOpMode.hardwareMap.get(DcMotor.class, "leftBack");
         rightBack = myOpMode.hardwareMap.get(DcMotor.class, "rightBack");
         leftFront = myOpMode.hardwareMap.get(DcMotor.class, "leftFront");
         rightFront = myOpMode.hardwareMap.get(DcMotor.class, "rightFront");
 
         leftFront.setDirection(DcMotor.Direction.FORWARD);
-        rightFront.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
         leftBack.setDirection(DcMotor.Direction.REVERSE);
         rightBack.setDirection(DcMotor.Direction.FORWARD);
 
@@ -67,12 +87,15 @@ public class MecanumHardware {
             rightBack.setPower(0);*/
     public void ForwardDistance(double Power, int distance, double diameter, int gear_reduction) {
 
+//        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZXY, AngleUnit.DEGREES);
+
+
         leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        int Distance = leftBack.getCurrentPosition() + (int) ((distance / (diameter * 3.14)) * 28 * gear_reduction);
+        int Distance = leftBack.getCurrentPosition() + (int) ((distance / (diameter * 3.14)) * 537.6);
 
         leftBack.setTargetPosition(Distance);
         leftFront.setTargetPosition(Distance);
@@ -91,16 +114,42 @@ public class MecanumHardware {
         runtime.reset();
         while (leftBack.isBusy() && rightBack.isBusy() && leftFront.isBusy() && rightFront.isBusy()) {
 
+//            if (angles.firstAngle < -5){
+//                leftBack.setPower(Math.abs(Power)+0.05);
+//                leftFront.setPower(Math.abs(Power)+0.05);
+//                rightBack.setPower(Math.abs(Power));
+//                rightFront.setPower(Math.abs(Power));
+//                myOpMode.telemetry.addData("Motor Powers", " %7d :%7d :%7d :%7d", leftBack.getPower(), leftFront.getPower(),rightBack.getPower(), rightFront.getPower());
+//                myOpMode.telemetry.update();
+//            } else if (angles.firstAngle > 5) {
+//                leftBack.setPower(Math.abs(Power));
+//                leftFront.setPower(Math.abs(Power));
+//                rightBack.setPower(Math.abs(Power)+0.05);
+//                rightFront.setPower(Math.abs(Power)+0.05);
+//                myOpMode.telemetry.addData("Motor Powers", " %7d :%7d :%7d :%7d", leftBack.getPower(), leftFront.getPower(),rightBack.getPower(), rightFront.getPower());
+//                myOpMode.telemetry.update();
+//            } else {
+//                leftBack.setPower(Math.abs(Power));
+//                leftFront.setPower(Math.abs(Power));
+//                rightBack.setPower(Math.abs(Power));
+//                rightFront.setPower(Math.abs(Power));
+//                myOpMode.telemetry.addData("Motor Powers", " %7d :%7d :%7d :%7d", leftBack.getPower(), leftFront.getPower(),rightBack.getPower(), rightFront.getPower());
+//                myOpMode.telemetry.update();
+//            }
+
             // Display it for the driver.
             myOpMode.telemetry.addData("Running to", " %7d :%7d", Distance, Distance);
             myOpMode.telemetry.addData("Currently at", " at %7d :%7d", leftBack.getCurrentPosition(), rightBack.getCurrentPosition());
             myOpMode.telemetry.update();
             myOpMode.sleep(1);
         }
-        leftBack.setPower(0);
+
         leftFront.setPower(0);
-        rightBack.setPower(0);
+        leftBack.setPower(0);
         rightFront.setPower(0);
+        rightBack.setPower(0);
+
+
     }
 
     public void ForwardTime(double Power, long Time) {
@@ -138,6 +187,40 @@ public class MecanumHardware {
     }
 
     public void sideDistance(double Power, int distance, double diameter, int gear_reduction) {
+        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        int Distance = leftBack.getCurrentPosition() + (int) ((distance / (diameter * 3.14)) * 537.6);
+
+        leftBack.setTargetPosition(-Distance);
+        leftFront.setTargetPosition(Distance);
+        rightBack.setTargetPosition(Distance);
+        rightFront.setTargetPosition(-Distance);
+
+        leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        leftFront.setPower(Power);
+        rightFront.setPower(-Power);
+        leftBack.setPower(-Power);
+        rightBack.setPower(Power);
+        runtime.reset();
+        while (leftBack.isBusy() && rightBack.isBusy() && leftFront.isBusy() && rightFront.isBusy()) {
+
+            // Display it for the driver.
+            myOpMode.telemetry.addData("Running to", " %7d :%7d", Distance, Distance);
+            myOpMode.telemetry.addData("Currently at", " at %7d :%7d", leftBack.getCurrentPosition(), rightBack.getCurrentPosition());
+            myOpMode.telemetry.update();
+            myOpMode.sleep(1);
+        }
+        leftBack.setPower(0);
+        leftFront.setPower(0);
+        rightBack.setPower(0);
+        rightFront.setPower(0);
 
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum_wheel_drive_teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum_wheel_drive_teleop.java
@@ -32,6 +32,8 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 /**
@@ -57,6 +59,8 @@ public class Mecanum_wheel_drive_teleop extends LinearOpMode {
     private DcMotor rightFront = null;
     private DcMotor leftFront = null;
     private DcMotor rightBack = null;
+    private DcMotor lift = null;
+    private Servo intake = null;
 
     @Override
     public void runOpMode() {
@@ -70,28 +74,50 @@ public class Mecanum_wheel_drive_teleop extends LinearOpMode {
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
         leftFront  = hardwareMap.get(DcMotor.class, "leftFront");
         rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        lift = hardwareMap.get(DcMotor.class, "lift");
+        intake = hardwareMap.get(Servo.class, "intake");
         double speed;
         double strafe;
         double turn;
+        double liftpower;
+        boolean clawpositon = true;
+
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
         leftFront.setDirection(DcMotor.Direction.FORWARD);
-        rightFront.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
         leftBack.setDirection(DcMotor.Direction.REVERSE);
         rightBack.setDirection(DcMotor.Direction.FORWARD);
+        lift.setDirection(DcMotor.Direction.FORWARD);
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intake.setDirection(Servo.Direction.FORWARD);
+
+        intake.setPosition(0);
+
+        waitForStart();
 
         // Wait for the game to start (driver presses PLAY)
-        waitForStart();
-        MecanumHardware robot = new MecanumHardware(this);
 
-        robot.init();
+
+
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+//            speed = -gamepad1.left_stick_y;
+//            turn = gamepad1.right_stick_x;
+//            strafe = gamepad1.left_stick_x;
+
+//            telemetry.addData("speed", speed);
+//            telemetry.addData("turn", turn);
+//            telemetry.addData("strafe", strafe);
+//            telemetry.update();
+
+
             speed = -gamepad1.left_stick_y;
             turn = gamepad1.right_stick_x;
             strafe = gamepad1.left_stick_x;
+
 
             double LB = speed + turn - strafe;
             double LF = speed + turn + strafe;
@@ -102,6 +128,27 @@ public class Mecanum_wheel_drive_teleop extends LinearOpMode {
             leftFront.setPower(LF);
             rightBack.setPower(RB);
             rightFront.setPower(RF);
+
+            if(gamepad1.dpad_up){
+                lift.setPower(1);
+            } else if (gamepad1.dpad_down){
+                lift.setPower(-1);
+            } else {
+                lift.setPower(0);
+            }
+
+
+            if (gamepad1.right_bumper){
+                intake.setPosition(180);
+
+            } else if (gamepad1.left_bumper) {
+                intake.setPosition(0);
+
+            }
+
+
+
+            //newcode
 
 
             //robot.ForwardTime(0.5,1000);
